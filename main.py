ad14ac96dcf8df8f9f64db4903049c9365de222f
HenryBass <henrywbass21@gmail.com>
2021-09-09 19:49:09 -0400

    Update main.py

--- main.py
+++ main.py
@@ -14,16 +14,20 @@ main_letter = input("Core Letter: ")
 blacklist += "-."
 
 for x in letters_list:
-  alphabet_list.remove(x)
+  try:
+    alphabet_list.remove(x)
+    print(alphabet_list)
+  except:
+    print("Letters Entered Incorrectly")
 
 blacklist += ("".join(alphabet_list))
 
 f = open("words.txt", "r") 
 i = 0
 
-time.sleep(10)
+size = sum(1 for _ in f)
 
-while i < 1000000:
+while i < size:
     line = f.readline()
     if main_letter in line and len(line) > 5 and any(x for x in blacklist if x in line) == False:
         print(line)
